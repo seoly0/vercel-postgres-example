4a52cf8533d00f1208e3ed0ccaa027a68bf6fa0e
seoly0 <seoly0.dev@gmail.com>
2024-03-22 20:05:24 +0900

    init

---
+++ api/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { sql } = require('@vercel/postgres')
+
+const app = express();
+app.use(express.json({ extended: true }))
+
+sql.connect().then(() => {
+  app.listen(3000, () => { })
+})
+
+app.get('/', (req, res) => res.send('Vercel Express Postgres CRUD example'))
+
+app.get('/users', async (req, res) => {
+  
+  const result = await sql`SELECT * FROM users`
+
+  res.json({
+    success: true,
+    data: result.rows,
+  })
+})
+
+app.get('/user/:userId', async (req, res) => {
+
+  const result = await sql`SELECT * FROM users WHERE id = ${req.params.userId} LIMIT 1`
+
+  res.json({
+    success: true,
+    data: result.rows[0],
+  })
+})
+
+app.post('/user', async (req, res) => {
+  const result = await sql`INSERT INTO users(name, email) VALUES(${req.body.name}, ${req.body.email}) RETURNING *`
+
+  res.json({
+    success: true,
+    data: result.rows[0],
+  })
+})
+
+app.patch('/user/:userId', async (req, res) => {
+
+  const selectResult = await sql`SELECT * FROM users WHERE id = ${req.params.userId} LIMIT 1`
+  const user = selectResult.rows[0]
+
+  const name = user.name === req.body.name ? req.body.name : req.body.name
+  const email = user.email === req.body.email ? req.body.email : req.body.email 
+
+  const updateResult = await sql`UPDATE users SET name = ${name}, email = ${email} WHERE id = ${req.params.userId} RETURNING *`
+
+  res.json({
+    success: true,
+    data: updateResult.rows[0]
+  })
+})
+
+app.delete('/user/:userId', async (req, res) => {
+
+  await sql`DELETE FROM users WHERE id = ${req.params.userId}`
+
+  res.json({
+    success: true,
+  })
+})
+
+module.exports = app
